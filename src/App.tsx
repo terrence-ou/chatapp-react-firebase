470de7a04646f3c96304ce960c82ef6ae5a09b92
terrence-ou <outerrencedl@gmail.com>
2024-04-26 16:19:45 -0400

    env variable setup updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log(import.meta.env.VITE_API_KEY);
+  console.log("env API: ", import.meta.env.VITE_API_KEY);
   return (
     <>
       <div>
